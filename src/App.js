ccdb978f24e898c8b3e29b37970faa7bbbc345d6
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-01-12 02:11:48 -0800

    Updated extractor and ignored extra files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react'
-<<<<<<< HEAD
 import { Route } from 'react-router-dom'
 import Navigation from './views/navigation/navigation'
-=======
->>>>>>> e51f546216339b7a668af7e5e592259b1a71f401
 import HomePage from './views/homepage/homepage';
+import CommentExtractor from './views/comment-extractor/comment-extractor'
 import './App.css';
 
 export class App extends React.Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
         <Route exact={false} path='/'>
           <Navigation />
         </Route>
         <Route exact path='/' component={HomePage} />
-=======
-        <HomePage />
->>>>>>> e51f546216339b7a668af7e5e592259b1a71f401
+        <Route exact path='/start' component={CommentExtractor} />
       </div>
     );
   }
