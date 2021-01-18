6bca7490884d6524e4a640942ae8047fc75c2a13
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-01-18 03:29:37 -0800

    added reducers, store, and middlewares with a working provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './redux/store'
+
 import App from './App';
+import './index.css';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
