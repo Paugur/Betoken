02c26d8830d906e60d0b005f669534bd7d075d8b
Aneesh-Pothuru <pothuruaneesh23@gmail,com>
2021-01-19 00:24:10 -0800

    testing new reducer and changed css in loading

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,5 +3,5 @@ import { combineReducers } from 'redux';
 import loadingReducer from './loading/loading.reducer'
 
 export default combineReducers({
-    loading: loadingReducer
+    loadingReducer
 })
