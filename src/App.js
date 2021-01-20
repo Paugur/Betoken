853ca72865769dc73f8a17328ee0d888351e3345
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-01-19 19:03:11 -0800

    Using hooks from react-redux and working logger, need to update App.js to work with state updates

--- src/App.js
+++ src/App.js
@@ -4,28 +4,28 @@ import Navigation from './views/navigation/navigation'
 import HomePage from './views/homepage/homepage';
 import CommentExtractor from './views/comment-extractor/comment-extractor'
 import LoadScreen from './views/load-screen/load-screen'
+import { useSelector } from 'react-redux';
 import './App.css';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        {
-          false
-            ?
-            <LoadScreen />
-            :
-            <div>
-              <Route exact={false} path='/'>
-                <Navigation />
-              </Route>
-              <Route exact path='/' component={HomePage} />
-              <Route exact path='/search' component={CommentExtractor} />
-            </div>
-        }
-      </div>
-    );
-  }
+export const App = () => {
+  const loading = useSelector(state => state.loading);
+  return (
+    <div className="App">
+      {
+        loading
+          ?
+          <LoadScreen />
+          :
+          <div>
+            <Route exact={false} path='/'>
+              <Navigation />
+            </Route>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/search' component={CommentExtractor} />
+          </div>
+      }
+    </div>
+  );
 }
 
 export default App;
