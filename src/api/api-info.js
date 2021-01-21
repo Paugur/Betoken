05b55215fa9284869e8d6f170bcc14d0f7217f63
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-01-21 01:50:39 -0800

    implemented axios and made a post request with beta query page

---
+++ src/api/api-info.js
@@ -0,0 +1,1 @@
+export const websitePrefix = "http://localhost:3000/";
--- src/views/comment-extractor/comment-extractor.js
+++ src/views/comment-extractor/comment-extractor.js
@@ -3,14 +3,26 @@ import Logo from '../../components/logo/logo'
 import Input from '../../components/input/input'
 import Button from '../../components/button/button'
 import { useDispatch, useSelector } from 'react-redux'
-import { startLoading } from '../../redux/loading/loading.actions'
+import { setApiKey } from '../../redux/user/user.actions'
+import { setMediaId } from '../../redux/media/media.actions'
+import { startLoading, cancelLoading } from '../../redux/loading/loading.actions'
+import { setComments } from '../../redux/media/media.actions'
+import { youtubeExtractor } from '../../api/comment-extractor/youtube-extractor'
 import './comment-extractor.css'
 
 
 const CommentExtractor = () => {
     const { mediaId } = useSelector(state => state.mediaReducer);
-    const { apiKey } = useSelector(state => state.userReducer)
+    const { apiKey } = useSelector(state => state.userReducer);
     const dispatch = useDispatch();
+
+    const onKeyChange = (event) => {
+        dispatch(setApiKey(event.target.value))
+    }
+
+    const onIdChange = (event) => {
+        dispatch(setMediaId(event.target.value))
+    }
 
     return (
         <div>
@@ -20,15 +32,28 @@ const CommentExtractor = () => {
                 type="text"
                 label="API Key"
                 value={apiKey}
+                inputChange={onKeyChange}
             />
             <Input
                 name="mediaId"
                 type="text"
                 label="Video ID"
                 value={mediaId}
+                inputChange={onIdChange}
             />
             <div className="center">
-                <Button onClick={() => dispatch(startLoading())} label="Query" />
+                <Button onClick={() => {
+                    dispatch(startLoading());
+                    youtubeExtractor(mediaId, apiKey).then(response => {
+                        response.json();
+                    }).then(comments => {
+                        dispatch(setComments(comments));
+                        dispatch(cancelLoading());
+                    }).catch(error => {
+                        dispatch(cancelLoading());
+                        alert('Something went wrong');
+                    })
+                }} label="Query" />
             </div>
         </div>
     )
