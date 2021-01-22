47501b4fc0560953af2a0dcaf262df30b90376dd
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-01-22 01:55:50 -0800

    Made it easier for other extractors to be added

---
+++ src/api/comment-extractor/youtube-extractor/youtube-extractor.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import axios from 'axios';
+import { websitePrefix } from '../../api-info'
+import Input from '../../../components/input/input'
+import Button from '../../../components/button/button'
+import { useDispatch, useSelector } from 'react-redux'
+import { setApiKey } from '../../../redux/user/user.actions'
+import { setMediaId } from '../../../redux/media/media.actions'
+import { startLoading, cancelLoading } from '../../../redux/loading/loading.actions'
+import { setComments, setVideoInfo, setCreatorInfo } from '../../../redux/media/media.actions'
+import './youtube-extractor.css'
+
+export const youtubeExtractor = (mediaId, apiKey) => {
+    return axios({
+        url: websitePrefix + 'api/youtube_extractor/' + mediaId + '/' + apiKey,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        mode: 'cors',
+    })
+}
+
+const YoutubeAPI = () => {
+    const { mediaId } = useSelector(state => state.mediaReducer);
+    const { apiKey } = useSelector(state => state.userReducer);
+    const dispatch = useDispatch();
+
+    const onKeyChange = (event) => {
+        dispatch(setApiKey(event.target.value))
+    }
+
+    const onIdChange = (event) => {
+        dispatch(setMediaId(event.target.value))
+    }
+
+    const onButtonRequest = () => {
+        dispatch(startLoading());
+        youtubeExtractor(mediaId, apiKey).then(response => {
+            dispatch(setVideoInfo(response.data.video));
+            dispatch(setCreatorInfo(response.data.channel));
+            dispatch(setComments(JSON.parse(response.data.comments)));
+            dispatch(cancelLoading());
+        }).catch(error => {
+            dispatch(cancelLoading());
+            alert('Something went wrong, Recorded ' + error);
+        })
+    }
+
+    return (
+        <div>
+            <Input
+                name='apiKey'
+                type="text"
+                label="API Key"
+                value={apiKey}
+                inputChange={onKeyChange}
+            />
+            <Input
+                name="mediaId"
+                type="text"
+                label="Video ID"
+                value={mediaId}
+                inputChange={onIdChange}
+            />
+            <div className="center">
+                <Button onClick={onButtonRequest} label="Query" />
+            </div>
+        </div>
+    )
+}
+
+export default YoutubeAPI;
