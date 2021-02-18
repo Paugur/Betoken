665aef19ad26ddfccf25579a02d4682bfa3a542b
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-02-18 11:22:34 -0800

    added input testing and tried to fix linter

--- src/views/footer/footer.styles.js
+++ src/views/footer/footer.styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const FooterContainer = styled.div`
+export const FooterContainer = styled.footer`
   position: absolute;
   bottom: 0;
   width: 100%;
--- src/views/navigation/__test__/navigation.test.js
+++ src/views/navigation/__test__/navigation.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { mount, render } from "enzyme";
+import { render } from "enzyme";
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import Navigation from "../navigation";
@@ -12,11 +12,11 @@ describe("Navigation", () => {
     const store = mockStore({
       mediaReducer: { comments: [] },
     });
-    const wrapper = mount(
+    const wrapper = render(
       <Provider store={store}>
         <Navigation />
       </Provider>
     );
-    render(wrapper);
+    expect(wrapper);
   });
 });
