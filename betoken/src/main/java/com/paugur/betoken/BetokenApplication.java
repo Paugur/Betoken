d62a6f8358578db933d89c2c3d17e99046dcbf0e
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-07-04 22:28:10 -0700

    Spring setup and folders reorganized

---
+++ betoken/src/main/java/com/paugur/betoken/BetokenApplication.java
@@ -0,0 +1,13 @@
+package com.paugur.betoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetokenApplication.class, args);
+	}
+
+}
---
+++ betoken/src/test/java/com/paugur/betoken/BetokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paugur.betoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
